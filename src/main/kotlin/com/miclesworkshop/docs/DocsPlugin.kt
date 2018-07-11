2d3aa39b407b1f1b71f1b83d1a0720d0bbc81310
MicleBrick <miclebrick@outlook.com>
2018-07-11 13:22:42 -0400

    Implemented /docs origin

--- src/main/kotlin/com/miclesworkshop/docs/DocsPlugin.kt
+++ src/main/kotlin/com/miclesworkshop/docs/DocsPlugin.kt
@@ -4,6 +4,7 @@ import co.aikar.commands.BaseCommand
 import org.bukkit.plugin.java.JavaPlugin
 import co.aikar.commands.PaperCommandManager
 import co.aikar.commands.CommandHelp
+import co.aikar.commands.InvalidCommandArgument
 import co.aikar.commands.annotation.*
 import org.bukkit.command.CommandSender
 import org.eclipse.jgit.api.Git
@@ -12,6 +13,7 @@ import java.io.File
 class DocsPlugin : JavaPlugin() {
     private val commandManager = PaperCommandManager(this)
     val gitRoot = File(dataFolder, "repository")
+    val gitRootTmp = File(dataFolder, "repository_tmp")
 
     override fun onEnable() {
         gitRoot.mkdirs()
@@ -35,13 +37,22 @@ class DocsCommand(val plugin: DocsPlugin) : BaseCommand() {
     @Subcommand("origin")
     @Description("Changes git origin URL and clones")
     @CommandPermission("docs.origin")
-    fun onOrigin() {
-//        val git = Git()
+    fun onOrigin(sender: CommandSender, url: String) {
+        plugin.gitRootTmp.mkdirs()
+        try {
+            Git.cloneRepository().setURI(url).setDirectory(plugin.gitRootTmp).call()
+            plugin.gitRoot.deleteRecursively()
+            plugin.gitRootTmp.renameTo(plugin.gitRoot)
+        } catch (e: Exception) {
+            plugin.gitRootTmp.deleteRecursively()
+            e.printStackTrace()
+            throw InvalidCommandArgument("Error occurred while attempting to clone: ${e.message}. See console for stacktrace.")
+        }
     }
 
     @Subcommand("update")
     @Description("updates the repo")
-    fun onUpdate() {
+    fun onUpdate(sender: CommandSender) {
 
     }
 }
